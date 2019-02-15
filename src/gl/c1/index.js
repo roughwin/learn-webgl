df7d699d10d557b5d806e62ccd7b6f12a2f90887
bowenqing <bowenqing@innobuddy.com>
2019-02-15 11:56:59 +0800

    add raw loader

--- src/gl/c1/index.js
+++ src/gl/c1/index.js
@@ -1,31 +1,9 @@
 import { createProgram, createShader } from '../common.js';
-import vertexSource from './vertex.glsl';
-
+import vertexShaderSource from './vertex.glsl';
+import fragmentShaderSource from './fragment.glsl';
 
 export default function c1(canvas) {
-  console.log(vertexSource)
   const gl = canvas.getContext('webgl');
-  const vertexShaderSource = `
-  // 一个属性变量，将会从缓冲中获取数据
-  attribute vec4 a_position;
- 
-  // 所有着色器都有一个main方法
-  void main() {
- 
-    // gl_Position 是一个顶点着色器主要设置的变量
-    gl_Position = a_position;
-  }
-  `;
-  const fragmentShaderSource = `
-  // 片断着色器没有默认精度，所以我们需要设置一个精度
-  // mediump是一个不错的默认值，代表“medium precision”（中等精度）
-  precision mediump float;
- 
-  void main() {
-    // gl_FragColor是一个片断着色器主要设置的变量
-    gl_FragColor = vec4(1, 0, 0.5, 1); // 返回“瑞迪施紫色”
-  }
-  `;
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
   const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
   var program = createProgram(gl, vertexShader, fragmentShader);
