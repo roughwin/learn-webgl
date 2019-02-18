539961667dd562046ea1a46fa9ee5bee8329de5e
bowenqing <bowenqing@innobuddy.com>
2019-02-18 19:01:53 +0800

    route list

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import App from './gl/App';
+import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+  <div>
+
+  <Link to="/gl">gl</Link>
+  <Link to="/three">three</Link>,
+    <Routes />
+  </div>
+  </Router>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/routes.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom'
+import { withRouter } from 'react-router'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+// import TransitionEx, { CssTransitionHOC } from 'components/common/react_transition';
+
+import Gl from './gl/App';
+import Three from './three';
+
+@withRouter
+class TransitionRoutes extends Component {
+  render() {
+    return <TransitionGroup
+      style={{
+        position: 'relative'
+      }}
+    >
+      <CSSTransition
+        timeout={{
+          enter: 800,
+          exit: 200,
+        }}
+        key={this.props.location.pathname}
+        classNames={{
+          enter: 'fadeInRight',
+          enterActive: 'animated',
+          exit: 'fadeOutLeft',
+          exitActive: 'animated',
+        }}
+      >
+        <div
+          style={{
+            position: 'absolute',
+            width: '100%'
+          }}
+        >
+          <Switch
+            location={this.props.location}
+          >
+           {
+             this.props.children
+           } 
+          </Switch>
+        </div>
+      </CSSTransition>
+    </TransitionGroup>
+  }
+}
+export default class Routes extends Component {
+  render() {
+    return <TransitionRoutes>
+      <Route path="/gl" component={Gl} />
+      <Route path="/three" component={Three} />
+    </TransitionRoutes>;
+  }
+}
